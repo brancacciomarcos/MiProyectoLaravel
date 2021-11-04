3302b0054a700c4ca0eef74a3c8814afc15476bb
unknown <brancacciomarcos@gmail.com>
2021-11-04 16:58:42 -0300

    amd

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,21 +3,26 @@
 <head>
     <title>@yield('title')</title>
     <style>
+        html, body {
+            margin: 0;
+            padding: 0;
+        }
         .active a {
             color:red;
             text-decoration: none;
         }
         .main {
-            background-image: url(https://www.google.com/imgres?imgurl=https%3A%2F%2Fimg.freepik.com%2Ffoto-gratis%2Ffondo-tecnologia-abstracta-codigo-programacion_34663-61.jpg%3Fsize%3D626%26ext%3Djpg&imgrefurl=https%3A%2F%2Fwww.freepik.es%2Ffotos-vectores-gratis%2Fprogramacion-web&tbnid=ay9QP36KWAMcmM&vet=12ahUKEwjSn-2Us__zAhXPJbkGHfLLCYgQMygBegUIARCzAQ..i&docid=zOEilJBf6WCWSM&w=626&h=521&q=imagenes%20programacion%20para%20usar%20de%20fondo%20en%20una%20pagina%20web&ved=2ahUKEwjSn-2Us__zAhXPJbkGHfLLCYgQMygBegUIARCzAQ)
+            background-image: url(https://img.freepik.com/foto-gratis/fondo-tecnologia-abstracta-codigo-programacion_34663-61.jpg?size=626&ext=jpg);
+            width: 100;
+            height: 100;
+            position: absolute;
         }
     </style>
     <link rel="stylesheet" href="css/stilo.css">
 </head>
 
-<body>
-    <div class="main">
+<body class="main">
         @include('partials.nav')
         @yield('content')
-    </div>
 </body>
 </html>
