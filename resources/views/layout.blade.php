9d4608dfd8da00eaeec2616871dc0b4e59dc9607
unknown <brancacciomarcos@gmail.com>
2021-11-04 16:19:32 -0300

    fondo

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html
+<html>
 <head>
     <title>@yield('title')</title>
     <style>
@@ -7,10 +7,17 @@
             color:red;
             text-decoration: none;
         }
+        .main {
+            background-image: url(https://www.google.com/imgres?imgurl=https%3A%2F%2Fimg.freepik.com%2Ffoto-gratis%2Ffondo-tecnologia-abstracta-codigo-programacion_34663-61.jpg%3Fsize%3D626%26ext%3Djpg&imgrefurl=https%3A%2F%2Fwww.freepik.es%2Ffotos-vectores-gratis%2Fprogramacion-web&tbnid=ay9QP36KWAMcmM&vet=12ahUKEwjSn-2Us__zAhXPJbkGHfLLCYgQMygBegUIARCzAQ..i&docid=zOEilJBf6WCWSM&w=626&h=521&q=imagenes%20programacion%20para%20usar%20de%20fondo%20en%20una%20pagina%20web&ved=2ahUKEwjSn-2Us__zAhXPJbkGHfLLCYgQMygBegUIARCzAQ)
+        }
     </style>
+    <link rel="stylesheet" href="css/stilo.css">
 </head>
+
 <body>
-    @include('partials.nav')
-    @yield('content')
+    <div class="main">
+        @include('partials.nav')
+        @yield('content')
+    </div>
 </body>
 </html>
