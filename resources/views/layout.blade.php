868365d7194b9cad7537ded6f55bb1d61c054f02
unknown <brancacciomarcos@gmail.com>
2021-11-17 23:49:22 -0300

    cambios en html

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,23 +2,26 @@
 <html>
 <head>
     <title>@yield('title')</title>
+    <meta charset="utf-8">
+    <meta name="keywords" content="Desarrollador Web, programador, back-end">
+    <meta name="description" content="Estudiante en desarrollo web en busqueda de empleo, demuestra sus capacidades basicas.">
+    <meta name="author" content="Brancaccio Marcos">
+    <link rel="stylesheet" href="css/app.css">
+    
     <style>
         html, body {
             margin: 0;
             padding: 0;
         }
         .active a {
-            color:red;
+            color:rgb(133, 17, 228);
             text-decoration: none;
         }
         .main {
             background-image: url(https://img.freepik.com/foto-gratis/fondo-tecnologia-abstracta-codigo-programacion_34663-61.jpg?size=626&ext=jpg);
-            width: 100;
-            height: 100;
-            position: absolute;
+            
         }
     </style>
-    <link rel="stylesheet" href="css/stilo.css">
 </head>
 
 <body class="main">
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,6 +1,10 @@
-<nav>
-    <ul>
-        <li class="{{ setActive('home') }}"><a href="{{ route('home') }}">@lang('Home')</a></li>
-        <li class="{{ setActive('contact') }}"><a href="{{ route('contact') }}">@lang('Contact')</a></li>
+<nav class="navbar bg-white shadow-sm" >
+    <ul class="nav">
+        <li class="nav-item {{ setActive('home') }}">
+            <a class="nav-link" href="{{ route('home') }}">
+                @lang('Home')
+            </a>
+        </li>
+        <li class="nav-item {{ setActive('contact') }}"><a href="{{ route('contact') }}">@lang('Contact')</a></li>
     </ul>
 </nav>
